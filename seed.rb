d12a09d379200bcbebf69b589593c27c1c9b403d
davidtwhitlatch <david.whitlatch@generalassem.ly.com>
2020-11-20 15:00:43 -0500

    Update seed.rb

--- seed.rb
+++ seed.rb
@@ -13,7 +13,7 @@ Dojo.destroy_all
   name: 'Johnny Lawrence',
   image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cobra-k-1526581667.jpg?resize=480:*',
   wise_quote: "I'm gonna teach you the style of karate that was taught to me. A method of fighting your pansy-ass generation desperately needs. I'm not just gonna teach you how to conquer your fears. I'm gonna teach you how to awaken the snake within you. And once you do that, you'll be the one who's feared. You'll build strength. You'll learn discipline. And when the time is right, you'll fight back.",
-  dojo: @dojo1
+  dojo: @cobra
 )
 
 @miyagi_do = Dojo.create!(name: 'Miyagi-do', motto: 'Inner peace, focus, balance.')
@@ -21,13 +21,13 @@ Dojo.destroy_all
   name: 'Mr. Miyagi',
   image_url: 'https://costumewall.com/wp-content/uploads/2018/10/mr-miyagi.jpg',
   wise_quote: 'For man with no forgiveness in heart, life worse punishment than death',
-  dojo: @dojo_2
+  dojo: @miyagi_do
 )
 @daniel = Sensei.create!(
   name: 'Daniel LaRusso',
   image_url: 'https://www.indiewire.com/wp-content/uploads/2018/06/02_CobraKai.jpg?w=780',
   wise_quote: 'Karate lies in the mind and heart. Not in the hands.',
-  dojo: @dojo_2
+  dojo: @miyagi_do
 )
 
 puts "#{Dojo.count} dojos created!"
